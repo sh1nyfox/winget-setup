b4dfab7bbcf6e6b2b00d2a2d0a5018ed94285eb6
sh1nyfox <sh1nyfox@protonmail.com>
2021-02-06 22:23:41 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## New PC setup using Windows Package Manager 
+
+Since Windows Package Manager now covers almost all of what I need on a PC at setup, I've decided to rewrite the useful setup script I previously had using Chocolatey. 
+
+Windows Package Manager transfers from machine to machine easily. Once Windows is up and running the AppInstaller just needs updating through the store first then this can be run in PowerShell. 
+
+Additionally, as I'm learning WSL (or trying to) the final portion will be to ensure virtualisation is enabled as well as the Windows Subsystem for Linux. Being last, hopefully a reboot will trigger and the whole thing will be completely silent and seamless.
+
+Or that's the plan anyway. 
+
+Credit to winstall.app for making this process much easier, if you're using Windows Package Manager it's an absolute essential. 
---
+++ setup.ps1
@@ -0,0 +1,18 @@
+#Setup script for PC using winget and enabling WSL
+#Thanks to winstall.app for making this easier
+#Must run in PowerShell as administrator (after updating AppInstaller from MS Store)
+
+#Windows Package Manager installs
+
+    winget install Valve.Steam  ; winget install Nvidia.GeForceNow  ; winget install OBSProject.OBSStudio  ; winget install Telegram.TelegramDesktop  ; winget install Mozilla.ThunderbirdBeta  ; winget install Sinew.Enpass  ; winget install Microsoft.PowerShell  ; winget install Microsoft.WindowsTerminal  ; winget install Canonical.Ubuntu  ; winget install EpicGames.EpicGamesLauncher  ; winget install Microsoft.PowerToys  ; winget install CrystalDewWorld.CrystalDiskMark  ; winget install PrimateLabs.Geekbench  ; winget install Notepads.Notepads  ; winget install Glimpse.Glimpse  ; winget install VideoLAN.VLC  ; winget install Oracle.VirtualBox  ; winget install GitHub.cli  ; winget install GitHub.GitHubDesktop  ; winget install Adobe.Brackets  ; winget install Ytmdesktop.Ytmdesktop   
+
+#Enabling WSL and virtualisation 
+
+    dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+    dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all 
+
+# Reboot PC
+
+    Restart-Computer
+
+    
