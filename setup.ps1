3b5ed076c132970f28b38c019a50a5cf94f37965
sh1nyfox <sh1nyfox@protonmail.com>
2021-11-03 23:03:45 +0000

    Updated WSL setup to new single command

--- setup.ps1
+++ setup.ps1
@@ -6,13 +6,7 @@
 
     winget install Valve.Steam  ; winget install Nvidia.GeForceNow  ; winget install OBSProject.OBSStudio  ; winget install Telegram.TelegramDesktop  ; winget install Sinew.Enpass  ; winget install Microsoft.PowerShell  ; winget install Microsoft.WindowsTerminal  ; winget install Canonical.Ubuntu  ; winget install EpicGames.EpicGamesLauncher  ; winget install Microsoft.PowerToys  ; winget install CrystalDewWorld.CrystalDiskMark  ; winget install PrimateLabs.Geekbench  ; winget install Notepads.Notepads  ; winget install VideoLAN.VLC  ; winget install GitHub.cli  ; winget install GitHub.GitHubDesktop ; winget install Mozilla.Firefox ; winget install Files-Community.Files ; winget install GIMP.GIMP 
 
-#Enabling WSL and virtualisation 
+# WSL setup 
 
-    dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-    dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all 
-
-# Reboot PC
-
-    Restart-Computer
-
+	wsl --install 
   
